52769de6723e654e68cf305546a006a3c9378e5a
Kamil Wójcik <voicik.kamil@gmail.com>
2023-07-09 17:47:32 +0200

    Added marketing info about items in cart

--- web_shop_vue/components/CartItem.vue
+++ web_shop_vue/components/CartItem.vue
@@ -36,6 +36,24 @@
           <Icon name="material-symbols:delete-outline" size="20" />
         </button>
       </div>
+      <div class="text-xl font-semibold">
+        $ <span class="font-bold">{{ product.price / 100 }}</span>
+      </div>
+
+      <p class="text-[#009A66] text-xs font-semibold pt-1">
+        Free 11-day delivery over ￡8.28
+      </p>
+
+      <p class="text-[#009A66] text-xs font-semibold pt-1">Free Shipping</p>
+
+      <div class="flex items-center justify-end">
+        <button
+          @click="removeFromCart()"
+          class="sm:hidden block -mt-0.5 hover:text-red-500"
+        >
+          <Icon name="material-symbols:delete-outline" size="20" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
