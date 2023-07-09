d3704d5209026672644698da5cc109282d73c492
Kamil Wójcik <voicik.kamil@gmail.com>
2023-07-09 13:31:48 +0200

    Added Product Component with examples

--- web_shop_vue/pages/index.vue
+++ web_shop_vue/pages/index.vue
@@ -1,10 +1,56 @@
 <template>
   <MainLayout>
-    <div d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+    <div d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div
+        class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
+      >
+        <div v-if="products" v-for="product in products" :key="product">
+          <ProductComponent :product="product" />
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
+
+const products = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/6/800/800",
+    price: 9899,
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description: "This is a description",
+    url: "https://picsum.photos/id/7/800/800",
+    price: 9999,
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    description: "This is a description",
+    url: "https://picsum.photos/id/9/800/800",
+    price: 289,
+  },
+  {
+    id: 4,
+    title: "Title 4",
+    description: "This is a description",
+    url: "https://picsum.photos/id/10/800/800",
+    price: 5999,
+  },
+  {
+    id: 5,
+    title: "Title 5",
+    description: "This is a description",
+    url: "https://picsum.photos/id/11/800/800",
+    price: 1999,
+  },
+];
 </script>
 <style lang="scss" scoped></style>
