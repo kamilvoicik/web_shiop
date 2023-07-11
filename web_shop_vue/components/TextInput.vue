bf7480f66c45623877aa6d5098b46d64b4770dbd
Kamil Wójcik <voicik.kamil@gmail.com>
2023-07-11 16:57:59 +0200

    Added TextInput Value

---
+++ web_shop_vue/components/TextInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+const emit = defineEmits(["update:input"]);
+
+const props = defineProps([
+  "input",
+  "placeholder",
+  "max",
+  "inputType",
+  "error",
+]);
+
+const { input, placeholder, max, inputType, error } = toRefs(props);
+let isFocusd = ref(false);
+
+const inputComputed = computed({
+  get: () => input.value,
+  set: (value) => emit("update:input", val),
+});
+</script>
+
+<style lang="scss" scoped></style>
