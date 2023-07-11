3309deb26b10ffcc36987ac8fb1a632c400fa0f4
Kamil Wójcik <voicik.kamil@gmail.com>
2023-07-11 16:57:45 +0200

    Added address page

---
+++ web_shop_vue/pages/address.vue
@@ -0,0 +1,15 @@
+<template>
+  <MainLayout>
+    <div id="AdressPage" class="mt-4 max-w-[500px] mx-auto px-2">
+      <div class="mx-auto bg-white rounded-lg p-3">
+        <div class="text-xl text-bold mb-2">Address Details</div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
+
+<style lang="scss" scoped></style>
