64450c9204fa6abdc476fe6ed3fc409eeed386a1
Kamil Wójcik <voicik.kamil@gmail.com>
2023-07-11 17:02:57 +0200

    Added simple component for text input

--- web_shop_vue/components/TextInput.vue
+++ web_shop_vue/components/TextInput.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <client-only>
+      <input
+        :placeholder="placeholder"
+        :maxlength="max"
+        class="w-full bg-white text-gray-800 border text-sm border-[#EFF0EB] rounded-lg p-3 placeholder-gray-500 focus:outline-none"
+        @focus="isFocused = true"
+        @blur="isFocused = false"
+        :class="({ 'border-gray-900': isFocused }, { 'border-red-500': error })"
+        :type="inputType"
+        v-model="inputComputed"
+        autocomplete="off"
+      />
+    </client-only>
+    <span v-if="error" class="text-red-500 text-[14px] font-semibold">
+      {{ error }}
+    </span>
+  </div>
 </template>
 
 <script setup>
@@ -14,7 +31,7 @@ const props = defineProps([
 ]);
 
 const { input, placeholder, max, inputType, error } = toRefs(props);
-let isFocusd = ref(false);
+let isFocused = ref(false);
 
 const inputComputed = computed({
   get: () => input.value,
