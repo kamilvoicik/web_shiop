2433de451de60570696b20642a28d4aee916a97e
Kamil Wójcik <72996480+kamilvoicik@users.noreply.github.com>
2023-07-14 17:34:54 +0200

    Added auth page

---
+++ web_shop_vue/pages/auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="AuthPage" class="w-full h-[100vh] bg-white">
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/AliExpress-logo.png">
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
