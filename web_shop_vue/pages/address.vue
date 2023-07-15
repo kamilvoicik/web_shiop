9a9a5182fe15971a79e4c60129b5c5544ec26e59
Kamil Wójcik <72996480+kamilvoicik@users.noreply.github.com>
2023-07-15 18:32:34 +0200

    Added updateing user address

--- web_shop_vue/pages/address.vue
+++ web_shop_vue/pages/address.vue
@@ -6,47 +6,19 @@
         <div class="mx-auto bg-white rounded-lg p-3">
           <div class="text-xl text-bold mb-2">Address Details</div>
           <form @submit.prevent="submit()">
-            <TextInput
-              class="w-full"
-              placeholder="Contact Name"
-              v-model:input="contactName"
-              inputType="text"
-              :error="error && error.type == 'contactName' ? error.message : ''"
-            />
-            <TextInput
-              class="w-full mt-2"
-              placeholder="Address"
-              v-model:input="address"
-              inputType="text"
-              :error="error && error.type == 'address' ? error.message : ''"
-            />
-            <TextInput
-              class="w-full mt-2"
-              placeholder="Zip Code"
-              v-model:input="zipCode"
-              inputType="text"
-              :error="error && error.type == 'zipCode' ? error.message : ''"
-            />
-            <TextInput
-              class="w-full mt-2"
-              placeholder="City"
-              v-model:input="city"
-              inputType="text"
-              :error="error && error.type == 'city' ? error.message : ''"
-            />
-            <TextInput
-              class="w-full mt-2"
-              placeholder="Country"
-              v-model:input="country"
-              inputType="text"
-              :error="error && error.type == 'country' ? error.message : ''"
-            />
+            <TextInput class="w-full" placeholder="Contact Name" v-model:input="contactName" inputType="text"
+              :error="error && error.type == 'contactName' ? error.message : ''" />
+            <TextInput class="w-full mt-2" placeholder="Address" v-model:input="address" inputType="text"
+              :error="error && error.type == 'address' ? error.message : ''" />
+            <TextInput class="w-full mt-2" placeholder="Zip Code" v-model:input="zipCode" inputType="text"
+              :error="error && error.type == 'zipCode' ? error.message : ''" />
+            <TextInput class="w-full mt-2" placeholder="City" v-model:input="city" inputType="text"
+              :error="error && error.type == 'city' ? error.message : ''" />
+            <TextInput class="w-full mt-2" placeholder="Country" v-model:input="country" inputType="text"
+              :error="error && error.type == 'country' ? error.message : ''" />
 
-            <button
-              :disabled="isWorking"
-              type="submit"
-              class="mt-6 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
-            >
+            <button :disabled="isWorking" type="submit"
+              class="mt-6 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full">
               <div v-if="!isWorking">Update Address</div>
               <Icon v-else name="eos-icons:loading" size="25" class="mr-2" />
             </button>
@@ -62,6 +34,7 @@ import MainLayout from "~/layouts/MainLayout.vue";
 import { useUserStore } from "~/stores/user";
 
 const userStore = useUserStore();
+const user = useSuperbaseUser();
 
 let contactName = ref(null);
 let address = ref(null);
@@ -74,7 +47,18 @@ let isUpdate = ref(false);
 let isWorking = ref(false);
 let error = ref(null);
 
-watchEffect(() => {
+watchEffect(async () => {
+  currentAddress.value = await useFetch(`/api/prisma/get-address-by-user/${user.value.id}`)
+
+  if (currentAddress.value.data) {
+    contactName.value = currentAddress.value.data.name
+    address.value = currentAddress.value.data.address
+    zipCode.value = currentAddress.value.data.zipcode
+    city.value = currentAddress.value.data.city
+    country.value = currentAddress.value.data.country
+
+    isUpdate.value = true
+  }
   userStore.isLoading = false;
 });
 
