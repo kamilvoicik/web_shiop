c211125753983b706bf90176cb7e6f59d6df284b
Kamil Wójcik <72996480+kamilvoicik@users.noreply.github.com>
2023-07-16 09:17:11 +0200

    Changed from dummy to user cart items

--- web_shop_vue/pages/shoppingcart.vue
+++ web_shop_vue/pages/shoppingcart.vue
@@ -1,15 +1,12 @@
 <template>
   <MainLayout>
     <div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-      <div v-if="false" class="h-[500px] flex items-center justify-center">
+      <div v-if="!userStore.cart.length" class="h-[500px] flex items-center justify-center">
         <div class="pt-20">
           <img class="mx-auto" width="250" src="/cart-empty.png" />
           <div class="text-xl text-center mt-4">No items yet?</div>
-          <div v-if="true" class="flex text-center">
-            <NuxtLink
-              to="/auth"
-              class="bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4"
-            >
+          <div v-if="!user" class="flex text-center">
+            <NuxtLink to="/auth" class="bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4">
               Sign in
             </NuxtLink>
           </div>
@@ -19,7 +16,7 @@
       <div v-else class="md:flex gap-4 justify-between mx-auto w-full">
         <div class="md:w-[65%]">
           <div class="bg-white rounded-lg p-4">
-            <div class="text-2xl font-bold mb-2">Shopping Cart ( 0 )</div>
+            <div class="text-2xl font-bold mb-2">Shopping Cart {{ userStore.cart.length }}</div>
           </div>
           <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
             <div class="text-red-500 font-bold">
@@ -27,12 +24,8 @@
             </div>
           </div>
           <div id="Items" class="bg-white rounded-lg p-4 mt-4">
-            <div v-for="product in products">
-              <CartItem
-                :product="product"
-                :selectedArray="selectedArray"
-                @selectedRadio="selectedRadioFunc"
-              />
+            <div v-for="product in userStore.cart">
+              <CartItem :product="product" :selectedArray="selectedArray" @selectedRadio="selectedRadioFunc" />
             </div>
           </div>
         </div>
@@ -48,10 +41,8 @@
               </div>
             </div>
 
-            <button
-              @click="goToCheckout"
-              class="flex items-center justify-center bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4"
-            >
+            <button @click="goToCheckout"
+              class="flex items-center justify-center bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4">
               Checkout
             </button>
           </div>
