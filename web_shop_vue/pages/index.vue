12fd48e3cf0103174e3c1d603e6afa613c117567
Kamil Wójcik <72996480+kamilvoicik@users.noreply.github.com>
2023-07-16 09:11:45 +0200

    Changed index items to db items

--- web_shop_vue/pages/index.vue
+++ web_shop_vue/pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <MainLayout>
     <div d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
-      <div
-        class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
-      >
-        <div v-if="products" v-for="product in products" :key="product">
+      <div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        <div v-if="products" v-for="product in products.data" :key="product">
           <ProductComponent :product="product" />
         </div>
       </div>
@@ -15,43 +13,15 @@
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
 import ProductComponent from "~/components/ProductComponent.vue";
+import { useUserStore } from "~/stores/user";
 
-const products = [
-  {
-    id: 1,
-    title: "Title 1",
-    description: "This is a description",
-    url: "https://picsum.photos/id/6/800/800",
-    price: 9899,
-  },
-  {
-    id: 2,
-    title: "Title 2",
-    description: "This is a description",
-    url: "https://picsum.photos/id/7/800/800",
-    price: 9999,
-  },
-  {
-    id: 3,
-    title: "Title 3",
-    description: "This is a description",
-    url: "https://picsum.photos/id/9/800/800",
-    price: 289,
-  },
-  {
-    id: 4,
-    title: "Title 4",
-    description: "This is a description",
-    url: "https://picsum.photos/id/10/800/800",
-    price: 5999,
-  },
-  {
-    id: 5,
-    title: "Title 5",
-    description: "This is a description",
-    url: "https://picsum.photos/id/11/800/800",
-    price: 1999,
-  },
-];
+const userStore = useUserStore();
+
+let products = ref(null)
+
+onBeforeMount(async () => {
+  products.value = await useFetch('/api/prisma/get-all-products')
+  setTimeout(() => userStore.isLoading = false, 1000)
+}) 
 </script>
 <style lang="scss" scoped></style>
